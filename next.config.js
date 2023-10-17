5cfa8a775f281c8d48fc8c52dab8a3d643feaf4b
AudunSorheim <audun.sorheim@nav.no>
2023-10-17 15:02:18 +0200

    Add dockerfile

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    basePath: "/oppfolgingsplan-lps-demo",
     pageExtensions: ["page.tsx", "page.ts", "page.js", "api.ts"],
     output: "standalone",
     productionBrowserSourceMaps: true,
