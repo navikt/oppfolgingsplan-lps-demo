d4747cad969f70c28333314bc94130fc33086b36
AudunSorheim <audun.sorheim@nav.no>
2024-02-14 14:37:39 +0100

    wip

--- src/app/api/submit/route.ts
+++ src/app/api/submit/route.ts
@@ -1,27 +1,24 @@
-import { NextRequest, NextResponse } from "next/server";
-import { get, post } from "@/server/axios-serverside";
-import {req} from "pino-std-serializers";
+import {NextRequest, NextResponse} from "next/server";
+import {get, post} from "@/server/axios-serverside";
 
 export async function POST(request: NextRequest) {
-  const basicUser = process.env.username;
-  const basicPass = process.env.password;
+    const basicUser = process.env.username;
+    const basicPass = process.env.password;
 
-  const base64Credentials = Buffer.from(`${basicUser}:${basicPass}`).toString(
-    "base64",
-  );
+    const base64Credentials = Buffer.from(`${basicUser}:${basicPass}`).toString(
+        "base64",
+    );
 
-  const maskinportenToken = await get(
-    "http://lps-oppfolgingsplan-mottak/api/test/token",
-    `Basic ${base64Credentials}`,
-  );
+    const maskinportenToken = await get(
+        "http://lps-oppfolgingsplan-mottak/api/test/token",
+        `Basic ${base64Credentials}`,
+    );
 
-  const plan = await request.json()
+    await post(
+        "http://lps-oppfolgingsplan-mottak/api/test/token",
+        request.body,
+        `Bearer ${maskinportenToken}`,
+    );
 
-  await post(
-    "http://lps-oppfolgingsplan-mottak/api/test/token",
-    plan,
-    `Bearer ${maskinportenToken}`,
-  );
-
-  return NextResponse.json({ message: "Woop" }, { status: 200 });
+    return NextResponse.json({message: "Woop"}, {status: 200});
 }
