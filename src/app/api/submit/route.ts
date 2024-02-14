fd717b37cb39fea567033f266d3da47af7e94384
AudunSorheim <audun.sorheim@nav.no>
2024-02-14 09:31:32 +0100

    wip

--- src/app/api/submit/route.ts
+++ src/app/api/submit/route.ts
@@ -1,11 +1,34 @@
-import {NextRequest, NextResponse} from "next/server";
-import {FollowUpPlanDTO} from "@/dto/FollowUpPlanDTO";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-    const oppfolgingsplan: FollowUpPlanDTO = await request.json();
+  const basicUser = process.env.username;
+  const basicPass = process.env.password;
 
-    const basicUser = process.env.username;
-    const basicPass = process.env.password;
+  const base64Credentials = Buffer.from(`${basicUser}:${basicPass}`).toString(
+    "base64",
+  );
 
-    return NextResponse.json({ message: "Woop" + basicUser }, { status: 200 });
+  const response = await fetch("/oppfolgingsplan-lps/api/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      // Add Authorization header
+      Authorization: `Basic ${base64Credentials}`,
+    },
+    body: request.body,
+  });
+
+  if (!response.ok) {
+    return NextResponse.json(
+      { message: "Failed to submit form" },
+      { status: 500 },
+    );
+  }
+
+  const maskinportenToken = await response.json();
+
+  return NextResponse.json(
+    { message: "Woop " + maskinportenToken },
+    { status: 200 },
+  );
 }
