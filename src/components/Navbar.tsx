b00e980cb50debc6ae4eccd8515cc5097211ca6f
André Wittwer <andre.wittwer@nav.no>
2025-12-16 09:05:08 +0100

    fix: add basepath to link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
-import Link from "next/link";
 import { Stepper } from "@navikt/ds-react";
+import Link from "next/link";
 
 interface Props {
   activeStep: number;
@@ -14,11 +14,15 @@ export const Navbar = ({ activeStep }: Props) => {
       orientation="horizontal"
       className="mb-8 mt-8 hidden md:flex"
     >
-      <Stepper.Step href="/">Oppfølgingsplan</Stepper.Step>
-      <Stepper.Step href="/infotilnavoglege">
+      <Stepper.Step as={Link} href="/">
+        Oppfølgingsplan
+      </Stepper.Step>
+      <Stepper.Step as={Link} href="/infotilnavoglege">
         Informasjon til NAV og lege
       </Stepper.Step>
-      <Stepper.Step href="/innsending">Innsending</Stepper.Step>
+      <Stepper.Step as={Link} href="/innsending">
+        Innsending
+      </Stepper.Step>
     </Stepper>
   );
 };
