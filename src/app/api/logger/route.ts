14b4a9b09a394a0f8e75c94b6fd0e86c2999fa58
André Wittwer <andre.wittwer@nav.no>
2025-12-16 08:21:51 +0100

    chore: remove codemod comment

--- src/app/api/logger/route.ts
+++ src/app/api/logger/route.ts
@@ -1,4 +1,1 @@
-export {
-  /* @next-codemod-error `POST` export is re-exported. Check if this component uses `params` or `searchParams`*/
-  POST,
-} from "@navikt/next-logger/app-dir";
+export { POST } from "@navikt/next-logger/app-dir";
